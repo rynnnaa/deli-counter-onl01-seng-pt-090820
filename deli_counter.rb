2a6640813e4a82506f4ddf37055f1c9f7a815587
Ryna Truong <rynatruong@outlook.com>
2020-10-31 14:15:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,8 +2,9 @@ require 'pry'
 katz_deli = []
   
   def line(katz_deli)
+    line = []
     if katz_deli.length > 0
-      katz_deli.map.with_index(1) do |person, idx|
+      katz_deli.each.with_index(1) do |person, idx|
         puts "The line is currently: #{idx}. #{person}"
     end
       else 
