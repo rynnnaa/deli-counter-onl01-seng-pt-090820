e0c0e3776b457b4bc37210218a1c50307081da94
Ryna Truong <rynatruong@outlook.com>
2020-10-31 14:41:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,7 +21,7 @@ katz_deli = []
       if katz_deli.empty?
         puts "There is nobody waiting to be served!"
       else
-        puts "Currently serving #{katz_deli}."
+        puts "Currently serving #{katz_deli.join}."
       end
     end
   end
