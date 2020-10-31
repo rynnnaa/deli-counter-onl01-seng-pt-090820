c915d649af95a2a1d235ab667c21eedc83ac414b
Ryna Truong <rynatruong@outlook.com>
2020-10-31 14:13:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+require 'pry'
+katz_deli = []
+  
+  def line(katz_deli)
+    if katz_deli.length > 0
+      katz_deli.map.with_index(1) do |person, idx|
+        puts "The line is currently: #{idx}. #{person}"
+    end
+      else 
+        puts "The line is currently empty."
+    end
+  end
