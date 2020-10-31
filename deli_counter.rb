812fad180c7a67e63b58b6590a7745e0dc50dfa1
Ryna Truong <rynatruong@outlook.com>
2020-10-31 14:40:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,7 +18,7 @@ katz_deli = []
     end
     
     def now_serving(katz_deli)
-      if line.empty?
+      if katz_deli.empty?
         puts "There is nobody waiting to be served!"
       else
         puts "Currently serving #{katz_deli}."
