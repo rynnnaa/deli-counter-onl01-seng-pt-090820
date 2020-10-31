2360a93a142d8b54825714fb9896c0aa13162669
Ryna Truong <rynatruong@outlook.com>
2020-10-31 14:23:02 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ katz_deli = []
       puts "The line is currently empty."
     else
       katz_deli.each.with_index(1) do |person, idx|
-        line.push "The line is currently: #{idx}. #{person}"
+        line.push("The line is currently: #{idx}. #{person}")
     end
       else 
         
